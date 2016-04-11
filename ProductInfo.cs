d0b8935ae4f64dd1e9cebc21d908feb4e771e5ea
andreaFIKT <andrea.stamenkovska@gmail.com>
2016-04-11 21:52:05 +0200

    #5

---
+++ ProductInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Факултет за информатички и комуникациски технологии - Битола")]
+[assembly: AssemblyProduct("LearnByPractice")]
+[assembly: AssemblyCopyright("Copyright © ФИКТ, 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
