6a59939d56af56d5c9a27058e7df600135029976
Jasmina_K <jasminakukunoska@gmail.com>
2016-04-13 10:36:52 +0200

    #4

--- Database/dbo/Tables/Vid_Organizacija.sql
+++ Database/dbo/Tables/Vid_Organizacija.sql
@@ -2,6 +2,6 @@
     [ID]  INT           IDENTITY (1, 1) NOT NULL,
     [Ime] NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_Vid_Organizacija] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [UK_Vid_Organizacija_Ime] UNIQUE NONCLUSTERED ([ID] ASC)
+    CONSTRAINT [UK_Vid_Organizacija_Ime] UNIQUE NONCLUSTERED ([Ime] ASC)
 );
 
