e1afdceb56f034308974610ba9bae20f2e425e0c
lencetanaskoska <lencetanaskoska@gmail.com>
2016-04-14 22:43:46 +0200

    #6 синхронизирано со Database

--- Domain/Practice/Prijava.cs
+++ Domain/Practice/Prijava.cs
@@ -5,6 +5,7 @@ namespace LearnByPractice.Domain.Practice
     /// <summary>Класа за пријава.</summary>
     public class Prijava
     {
+        private DateTime datum;
         /// <summary>Шифра </summary>
         /// <value>Ја зема/поставува шифрата на пријавата</value>
         public int Id { get; set; }
@@ -12,6 +13,14 @@ namespace LearnByPractice.Domain.Practice
         /// <summary>Шифра на комапнијата </summary>
         /// <value>Ја зема/поставува шифрата на компанијата</value>
         public int IdKompanija { get; set; }
+
+        /// <summary>Датум на пријавата</summary>
+        /// <value>Го зема/поставува датумот на пријавата</value>
+        public String Datum
+        {
+            set { DateTime.TryParse(value, out datum); }
+            get { return datum.Date.ToString(); }
+        }
 
         /// <summary> Конструктор на класата <c>Prijava</c>, без параметри. </summary>
         public Prijava() { }
