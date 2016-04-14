e9a450495fafa69bf2142fa4173cac99448045e8
Jasmina_K <jasminakukunoska@gmail.com>
2016-04-14 02:27:09 +0200

    #4

--- Database/dbo/Tables/Vid_Organizacija.sql
+++ Database/dbo/Tables/Vid_Organizacija.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[Vid_Organizacija] (
-    [ID]  INT           IDENTITY (1, 1) NOT NULL,
+    [ID]  INT           NOT NULL,
     [Ime] NVARCHAR (50) NOT NULL,
     CONSTRAINT [PK_Vid_Organizacija] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [UK_Vid_Organizacija_Ime] UNIQUE NONCLUSTERED ([Ime] ASC)
