e8b2083199c1ea6a26969e69f85ff7f9d7b0e6cf
Jasmina_K <jasminakukunoska@gmail.com>
2016-04-15 16:46:06 +0200

    #4

--- Database/dbo/Tables/Ocenka.sql
+++ Database/dbo/Tables/Ocenka.sql
@@ -3,10 +3,12 @@
     [Predmet_ID]  INT NOT NULL,
     [Ocenka]      INT NOT NULL,
     CONSTRAINT [PK_Ocenka] PRIMARY KEY CLUSTERED ([Korisnik_ID] ASC, [Predmet_ID] ASC),
-    CONSTRAINT [CK_Ocenka_Ocenka] CHECK ([Ocenka] like '[6-10]'),
-    CONSTRAINT [FK_Ocenka_Korinik] FOREIGN KEY ([Korisnik_ID]) REFERENCES [dbo].[Korisnik] ([ID]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [CK_Ocenka_Ocenka] CHECK ([Ocenka]>=(6) AND [Ocenka]<=(10)),
+    CONSTRAINT [FK_Ocenka_Korisnik] FOREIGN KEY ([Korisnik_ID]) REFERENCES [dbo].[Korisnik] ([ID]) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT [FK_Ocenka_Predmet] FOREIGN KEY ([Predmet_ID]) REFERENCES [dbo].[Predmet] ([ID])
 );
+
+
 
 
 
--- Database/dbo/Tables/Prijava_Korisnik.sql
+++ Database/dbo/Tables/Prijava_Korisnik.sql
@@ -2,9 +2,11 @@
     [Prijava_ID]  INT NOT NULL,
     [Korisnik_ID] INT NOT NULL,
     CONSTRAINT [PK_Prijava_Korisnik] PRIMARY KEY CLUSTERED ([Prijava_ID] ASC, [Korisnik_ID] ASC),
-    CONSTRAINT [FK_Prijava_Korisnik_Korinik] FOREIGN KEY ([Korisnik_ID]) REFERENCES [dbo].[Korisnik] ([ID]),
+    CONSTRAINT [FK_Prijava_Korisnik_Korisnik] FOREIGN KEY ([Korisnik_ID]) REFERENCES [dbo].[Korisnik] ([ID]),
     CONSTRAINT [FK_Prijava_Korisnik_Prijava] FOREIGN KEY ([Prijava_ID]) REFERENCES [dbo].[Prijava] ([ID])
 );
+
+
 
 
 
