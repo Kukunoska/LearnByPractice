2ac7894737eff0a18f8683295d67934ff60b3158
marija8888 <m.kuzmanovska8@gmail.com>
2016-04-17 01:57:38 +0200

    #13

---
+++ BLL/Managers/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnByPractice.BLL.Managers
+{
+    public abstract class ManagerBase
+    {
+        public int OcenaManager();
+        public int PredmetManager();
+        public int StudiskaProgramaManager();
+        public int KompanijaManager();
+        public int OblastManager();
+        public int PrijavaManager();
+        public int PrijavaStudentManager();
+        public int PrijavaTehnologijaManaager();
+        public int TehnologijaManager();
+        public int KorisnikManager();
+
+        
+    }
+}
