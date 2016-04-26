b3c0e6fea79c8c57577e702972c387f3a7ccf156
lencetanaskoska <lencetanaskoska@gmail.com>
2016-04-26 20:06:59 +0200

    Идеја околу изгледот на мастер страната, додадени страните за Администратор, Студент, Ментор

--- UI.Web/PublicPages/Proekti.aspx.cs
+++ UI.Web/PublicPages/Proekti.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace LearnByPractice.UI.Web
+namespace LearnByPractice.UI.Web.PublicPages
 {
-    public partial class WebSite : System.Web.UI.MasterPage
+    public partial class Proekti : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- UI.Web/User/Admnistrator.aspx.cs
+++ UI.Web/User/Admnistrator.aspx.cs
@@ -5,9 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace LearnByPractice.UI.Web
+namespace LearnByPractice.UI.Web.User
 {
-    public partial class WebSite : System.Web.UI.MasterPage
+    public partial class Admnistrator : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
