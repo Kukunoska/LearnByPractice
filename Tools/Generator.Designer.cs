9f29f6edee9420d6afe4807d7106feefdf2aab63
andreaFIKT <andrea.stamenkovska@gmail.com>
2016-04-27 21:21:54 +0200

    #16 поправено

--- Tools/Generator.Designer.cs
+++ Tools/Generator.Designer.cs
@@ -31,10 +31,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtVkupno = new System.Windows.Forms.TextBox();
-            this.txtZenski = new System.Windows.Forms.TextBox();
-            this.txtMashki = new System.Windows.Forms.TextBox();
             this.btnGeneriraj = new System.Windows.Forms.Button();
+            this.numZenski = new System.Windows.Forms.NumericUpDown();
+            this.numVkupno = new System.Windows.Forms.NumericUpDown();
+            this.numMashki = new System.Windows.Forms.NumericUpDown();
+            this.txtRezultati = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numZenski)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVkupno)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMashki)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -64,27 +68,6 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Колку машки податоци сакате да бидат изгенерирани:";
             // 
-            // txtVkupno
-            // 
-            this.txtVkupno.Location = new System.Drawing.Point(370, 10);
-            this.txtVkupno.Name = "txtVkupno";
-            this.txtVkupno.Size = new System.Drawing.Size(100, 20);
-            this.txtVkupno.TabIndex = 0;
-            // 
-            // txtZenski
-            // 
-            this.txtZenski.Location = new System.Drawing.Point(370, 43);
-            this.txtZenski.Name = "txtZenski";
-            this.txtZenski.Size = new System.Drawing.Size(100, 20);
-            this.txtZenski.TabIndex = 1;
-            // 
-            // txtMashki
-            // 
-            this.txtMashki.Location = new System.Drawing.Point(370, 76);
-            this.txtMashki.Name = "txtMashki";
-            this.txtMashki.Size = new System.Drawing.Size(100, 20);
-            this.txtMashki.TabIndex = 2;
-            // 
             // btnGeneriraj
             // 
             this.btnGeneriraj.Location = new System.Drawing.Point(501, 39);
@@ -95,20 +78,83 @@
             this.btnGeneriraj.UseVisualStyleBackColor = true;
             this.btnGeneriraj.Click += new System.EventHandler(this.btnGeneriraj_Click);
             // 
+            // numZenski
+            // 
+            this.numZenski.Location = new System.Drawing.Point(370, 42);
+            this.numZenski.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numZenski.Name = "numZenski";
+            this.numZenski.Size = new System.Drawing.Size(120, 20);
+            this.numZenski.TabIndex = 4;
+            this.numZenski.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // numVkupno
+            // 
+            this.numVkupno.Location = new System.Drawing.Point(370, 11);
+            this.numVkupno.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numVkupno.Name = "numVkupno";
+            this.numVkupno.Size = new System.Drawing.Size(120, 20);
+            this.numVkupno.TabIndex = 5;
+            // 
+            // numMashki
+            // 
+            this.numMashki.Location = new System.Drawing.Point(370, 83);
+            this.numMashki.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numMashki.Name = "numMashki";
+            this.numMashki.Size = new System.Drawing.Size(120, 20);
+            this.numMashki.TabIndex = 6;
+            this.numMashki.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // txtRezultati
+            // 
+            this.txtRezultati.AcceptsReturn = true;
+            this.txtRezultati.AcceptsTab = true;
+            this.txtRezultati.Location = new System.Drawing.Point(19, 126);
+            this.txtRezultati.Multiline = true;
+            this.txtRezultati.Name = "txtRezultati";
+            this.txtRezultati.ReadOnly = true;
+            this.txtRezultati.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRezultati.Size = new System.Drawing.Size(557, 20);
+            this.txtRezultati.TabIndex = 7;
+            this.txtRezultati.Text = "Генерирани корисници:";
+            // 
             // Generator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 262);
+            this.ClientSize = new System.Drawing.Size(624, 325);
+            this.Controls.Add(this.txtRezultati);
+            this.Controls.Add(this.numMashki);
+            this.Controls.Add(this.numVkupno);
+            this.Controls.Add(this.numZenski);
             this.Controls.Add(this.btnGeneriraj);
-            this.Controls.Add(this.txtMashki);
-            this.Controls.Add(this.txtZenski);
-            this.Controls.Add(this.txtVkupno);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Generator";
             this.Text = "Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.numZenski)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVkupno)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMashki)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,9 +165,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtVkupno;
-        private System.Windows.Forms.TextBox txtZenski;
-        private System.Windows.Forms.TextBox txtMashki;
         private System.Windows.Forms.Button btnGeneriraj;
+        private System.Windows.Forms.NumericUpDown numZenski;
+        private System.Windows.Forms.NumericUpDown numVkupno;
+        private System.Windows.Forms.NumericUpDown numMashki;
+        private System.Windows.Forms.TextBox txtRezultati;
     }
 }
--- Tools/Generator.cs
+++ Tools/Generator.cs
@@ -19,73 +19,81 @@ namespace Tools
         }
         private void btnGeneriraj_Click(object sender, EventArgs e)
         {
-            int a;
-            bool aA = int.TryParse(txtVkupno.Text, out a);
-            int b;
-            bool bB = int.TryParse(txtZenski.Text, out b);
-            int c;
-            bool cC = int.TryParse(txtMashki.Text, out c);
-
-
-            //Za Zhenski Iminja
-            string[] zi = File.ReadAllLines("C:\\Users\\Andrea\\Desktop\\NBD_FIKT\\LearnByPractice\\Tools\\ZenskiIminja.txt");
-            List<string> zenskiIminjaList = new List<string>();
-            string s1 = zi[0];
-            string[] zenskiIminja = s1.Split(',');
-            foreach (var zI in zenskiIminja)
+            int vkupnoKorisnici = Convert.ToInt32(numVkupno.Value);
+            int maskiKorisnici = Convert.ToInt32(numMashki.Value);
+            int zenskiKorisnici = Convert.ToInt32(numZenski.Value);
+            string path = Directory.GetCurrentDirectory();
+            try
             {
-                zenskiIminjaList.Add(zI);
-            }
-
-            //Za Zhenski Preziminja
-            string[] zp = File.ReadAllLines("C:\\Users\\Andrea\\Desktop\\NBD_FIKT\\LearnByPractice\\Tools\\ZenskiPreziminja.txt");
-            List<string> zenskiPreziminjaList = new List<string>();
-            string s2 = zp[0];
-            string[] zenskiPreziminja = s2.Split(',');
-            foreach (var zP in zenskiPreziminja)
-            {
-                zenskiPreziminjaList.Add(zP);
-            }
-
-            //Za Mashki Iminja
-            string[] mi = File.ReadAllLines("C:\\Users\\Andrea\\Desktop\\NBD_FIKT\\LearnByPractice\\Tools\\MashkiIminja.txt");
-            List<string> mashkiIminjaList = new List<string>();
-            string s3 = mi[0];
-            string[] mashkiIminja = s3.Split(',');
-            foreach (var mI in mashkiIminja)
-            {
-                mashkiIminjaList.Add(mI);
-            }
-
-            //Za Mashki Preziminja
-            string[] mp = File.ReadAllLines("C:\\Users\\Andrea\\Desktop\\NBD_FIKT\\LearnByPractice\\Tools\\MashkiPreziminja.txt");
-            List<string> mashkiPreziminjaList = new List<string>();
-            string s4 = mp[0];
-            string[] mashkiPreziminja = s4.Split(',');
-            foreach (var mP in mashkiPreziminja)
-            {
-                mashkiPreziminjaList.Add(mP);
-            }
-
-            Random randoM = new Random();
-            List<Korisnik> korisnici = new List<Korisnik>();
-            for (int i = 0; i < a; i++)
-            {
-                for (int j = 0; j < c; j++)
+                //Za Zhenski Iminja
+                string[] zenski_iminja = File.ReadAllLines(path + "\\ZenskiIminja.txt");
+                List<string> zenskiIminjaList = new List<string>();
+                string zenskoIme = zenski_iminja[0];
+                string[] zenskiIminja = zenskoIme.Split(',');
+                foreach (var zIme in zenskiIminja)
                 {
-
-                    Korisnik korisnikM = new Korisnik();
-                    korisnikM.Ime = mashkiIminjaList[randoM.Next(0, (mashkiIminjaList.Count - 1))];
-                    korisnikM.Prezime = mashkiPreziminjaList[randoM.Next(0, (mashkiPreziminjaList.Count - 1))];
-                    korisnici.Add(korisnikM);
+                    zenskiIminjaList.AddRange(zenskiIminja);
                 }
-                for (int j = 0; j < b; j++)
+
+                //Za Zhenski Preziminja
+                string[] zenski_preziminja = File.ReadAllLines(path + "\\ZenskiPreziminja.txt");
+                List<string> zenskiPreziminjaList = new List<string>();
+                string zenskoPrezime = zenski_preziminja[0];
+                string[] zenskiPreziminja = zenskoPrezime.Split(',');
+                foreach (var zPrezime in zenskiPreziminja)
+                {
+                    zenskiPreziminjaList.AddRange(zenskiPreziminja);
+                }
+
+                //Za Mashki Iminja
+                string[] mashki_iminja = File.ReadAllLines(path + "\\MashkiIminja.txt");
+                List<string> mashkiIminjaList = new List<string>();
+                string mashkoIme = mashki_iminja[0];
+                string[] mashkiIminja = mashkoIme.Split(',');
+                foreach (var mIme in mashkiIminja)
+                {
+                    mashkiIminjaList.AddRange(mashkiIminja);
+                }
+
+                //Za Mashki Preziminja
+                string[] mashki_preziminja = File.ReadAllLines(path + "\\MashkiPreziminja.txt");
+                List<string> mashkiPreziminjaList = new List<string>();
+                string mashkoPrezime = mashki_preziminja[0];
+                string[] mashkiPreziminja = mashkoPrezime.Split(',');
+                foreach (var mP in mashkiPreziminja)
+                {
+                    mashkiPreziminjaList.AddRange(mashkiPreziminja);
+                }
+
+                Random random = new Random();
+                List<Korisnik> korisnici = new List<Korisnik>();
+                for (int j = 0; j <vkupnoKorisnici-zenskiKorisnici; j++)
+                {
+                    Korisnik korisnikM = new Korisnik();
+                    korisnikM.Ime = mashkiIminjaList[random.Next(0, mashkiIminjaList.Count)];
+                    korisnikM.Prezime = mashkiPreziminjaList[random.Next(0, mashkiPreziminjaList.Count)];
+                    korisnici.Add(korisnikM);
+
+                }
+                for (int k = 0; k <zenskiKorisnici; k++)
                 {
                     Korisnik korisnikZ = new Korisnik();
-                    korisnikZ.Ime = zenskiIminjaList[randoM.Next(0, (zenskiIminjaList.Count - 1))];
-                    korisnikZ.Prezime = zenskiPreziminjaList[randoM.Next(0, (zenskiPreziminjaList.Count - 1))];
+                    korisnikZ.Ime = zenskiIminjaList[random.Next(0, zenskiIminjaList.Count)];
+                    korisnikZ.Prezime = zenskiPreziminjaList[random.Next(0, zenskiPreziminjaList.Count)];
                     korisnici.Add(korisnikZ);
                 }
+                foreach (var korisnik in korisnici)
+                {
+                    txtRezultati.AppendText(korisnik.Ime + korisnik.Prezime + " ");
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.ToString();
+            }
+            finally
+            {
+                MessageBox.Show("Успешно е направено генерирањето", "Браво", MessageBoxButtons.OK);
             }
         }
     }
