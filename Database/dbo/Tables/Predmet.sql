d533f0e3377343394de3dcf70a94f4ec9220516e
Jasmina_K <jasminakukunoska@gmail.com>
2016-05-04 06:16:21 +0200

    #4 Измени во табелите

--- Database/dbo/Tables/Predmet.sql
+++ Database/dbo/Tables/Predmet.sql
@@ -1,9 +1,15 @@
 ﻿CREATE TABLE [dbo].[Predmet] (
-    [ID]  INT           IDENTITY (1, 1) NOT NULL,
-    [Ime] NVARCHAR (50) NOT NULL,
-    CONSTRAINT [PK_Predmet] PRIMARY KEY CLUSTERED ([ID] ASC),
-    CONSTRAINT [UK_Predmet_Ime] UNIQUE NONCLUSTERED ([Ime] ASC)
+    [ID]                INT           IDENTITY (1, 1) NOT NULL,
+    [Shifra_Na_Predmet] NVARCHAR (10) NOT NULL,
+    [Ime]               NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Predmet] PRIMARY KEY CLUSTERED ([ID] ASC, [Shifra_Na_Predmet] ASC),
+    CONSTRAINT [UK_Predmet_Ime] UNIQUE NONCLUSTERED ([Ime] ASC),
+    CONSTRAINT [UK_Predmet_Shifra_Na_Predmet] UNIQUE NONCLUSTERED ([Shifra_Na_Predmet] ASC)
 );
+
+
+
+
 
 
 
