0e7d7a6c66312e6f1bc6a5ed9f1b021f54f88077
marija8888 <m.kuzmanovska8@gmail.com>
2016-05-04 05:30:22 +0200

    #13 тест метод за вид на организација (колку функционира незнам се скаравме со nuunit)

---
+++ Tests/BLL/Organizational/VidOrganizacijaManagerTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using LearnByPractice.BLL.Managers.Organizational;
+
+namespace LearnByPractice.Tests.BLL.Organizational
+{
+    [TestFixture]
+    public class VidOrganizacijaManagerTest
+    {
+        [Test]
+        public void VidOrganizacijaManagerTestRead()
+        {
+            VidOrganizacijaManager read = new VidOrganizacijaManager();
+            Assert.IsTrue(true);
+        }
+    }
+}
