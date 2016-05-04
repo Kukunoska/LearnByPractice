72a8e8bf3a222529f02be450686e1dd40d053156
lencetanaskoska <lencetanaskoska@gmail.com>
2016-05-04 17:28:54 +0200

    .

--- DAL.Models/LearnByPractice.designer.cs
+++ DAL.Models/LearnByPractice.designer.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DAL.Models
+namespace LearnByPractice.DAL.Models
 {
 	using System.Data.Linq;
 	using System.Data.Linq.Mapping;
