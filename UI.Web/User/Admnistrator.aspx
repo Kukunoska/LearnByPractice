1652cc90c25ba2f152a1c5983630a8f2d1b7afe0
lencetanaskoska <lencetanaskoska@gmail.com>
2016-05-10 23:25:39 +0200

    Прочитани поратоци од база

--- UI.Web/User/Admnistrator.aspx
+++ UI.Web/User/Admnistrator.aspx
@@ -1,6 +1,16 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Admnistrator.aspx.cs" Inherits="LearnByPractice.UI.Web.User.Admnistrator" MasterPageFile="~/WebSite.Master" Title="LearnByPractice - Администратор" %>
 
  <asp:Content runat="server" ContentPlaceHolderID="MainContentPlaceHolder" ID="MainContent">
-         <p>Страна за администратор</p>
-     </asp:Content>
+        <div id ="DvidOrganizacija">
+            <asp:Label ID="VidOrganizacijaNaslov" runat="server" Text="Видови Организации"></asp:Label> <br />
+     <asp:Label ID="VidO" runat="server"></asp:Label><br /><br />
+           </div>
+      <div id ="Dorganizacija">
+     <asp:Label ID="Organiz" runat="server"></asp:Label>
+             </div>
+      <div id ="DSP">
+          <asp:Label ID="DspNaslov" runat="server" Text="Студиски програми" ></asp:Label><br/>
+     <asp:Label ID="SP" runat="server"></asp:Label>
+             </div>
+ </asp:Content>
 
--- UI.Web/User/Admnistrator.aspx.designer.cs
+++ UI.Web/User/Admnistrator.aspx.designer.cs
@@ -11,5 +11,50 @@ namespace LearnByPractice.UI.Web.User {
     
     
     public partial class Admnistrator {
+        
+        /// <summary>
+        /// VidOrganizacijaNaslov control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label VidOrganizacijaNaslov;
+        
+        /// <summary>
+        /// VidO control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label VidO;
+        
+        /// <summary>
+        /// Organiz control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Organiz;
+        
+        /// <summary>
+        /// DspNaslov control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DspNaslov;
+        
+        /// <summary>
+        /// SP control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label SP;
     }
 }
