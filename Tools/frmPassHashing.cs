554835627bfb225818fca6108a33b234341ba3f4
Jasmina_K <jasminakukunoska@gmail.com>
2016-05-14 01:02:16 +0200

    #17 Нова класа за хаширање и нова форма

---
+++ Tools/frmPassHashing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tools;
+
+namespace LearnByPractice
+{
+    public partial class frmPassHashing : Form
+    {
+        public frmPassHashing()
+        {
+            InitializeComponent();
+        }
+        public frmPassHashing(frmMain parent) 
+        {
+            InitializeComponent(); 
+            MdiParent = parent; 
+        }
+
+        private void btnHashing_Click(object sender, EventArgs e)
+        {
+            txtHash.Text = Hashing.ComputeHash(txtPassword.Text, Supported_HASH.SHA256, null);
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = (Hashing.Confirm(txtPassword.Text, txtHash.Text, Supported_HASH.SHA256)) ? "Статус: Точно" : "Статус: Неточно";
+        }
+    }
+}
