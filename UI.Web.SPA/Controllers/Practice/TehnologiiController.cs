da7685c3f2d82e819e3349809482c123f500790d
Jasmina_K <jasminakukunoska@gmail.com>
2017-05-15 16:48:34 +0200

    Додавање на контролер класи за Област и Технологија

---
+++ UI.Web.SPA/Controllers/Practice/TehnologiiController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+using domain = LearnByPractice.Domain;
+using managers = LearnByPractice.BLL.Managers;
+
+namespace LearnByPractice.UI.Web.Controllers.Practice
+{
+    public class TehnologiiController : ControllerBase
+    {
+        [Route("api/practice/tehnologija/site")]
+        [HttpGet]
+        public IEnumerable<domain::Practice.Tehnologija> site()
+        {
+            ProveriDaliImaKorisnik();
+
+            var tehnologiiManager = new managers.Practice.TehnologijaManager();
+            var result = tehnologiiManager.GetAll();
+            return result;
+        }
+
+        [Route("api/practice/tehnologija/nov")]
+        [HttpPost]
+        public domain::Practice.Tehnologija nov(domain::Practice.Tehnologija tehnologija)
+        {
+            ProveriDaliImaKorisnik();
+
+            var tehnologiiManager = new managers.Practice.TehnologijaManager();
+            var result = tehnologiiManager.Insert(tehnologija);
+            return result;
+        }
+
+        [Route("api/practice/tehnologija/Izmeni")]
+        [HttpPost]
+        public domain::Practice.Tehnologija izmeni(domain::Practice.Tehnologija tehnologija)
+        {
+            ProveriDaliImaKorisnik();
+
+            var tehnologiiManager = new managers.Practice.TehnologijaManager();
+            var result = tehnologiiManager.Update(tehnologija);
+            return result;
+        }
+    }
+}
