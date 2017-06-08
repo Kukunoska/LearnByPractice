baeb19372bdbb2f3f6c0ba97e0d6b6152c2c17a4
Jasmina_K <jasminakukunoska@gmail.com>
2017-06-08 20:11:58 +0200

    За регистрирање

--- UI.Web.SPA/Index.aspx.designer.cs
+++ UI.Web.SPA/Index.aspx.designer.cs
@@ -85,6 +85,15 @@ namespace LearnByPractice.UI.Web.wwwroot {
         protected global::System.Web.UI.WebControls.Label NajavaPorakaZaGreshkaLabel;
         
         /// <summary>
+        /// imeLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label imeLabel;
+        
+        /// <summary>
         /// imeTextBox control.
         /// </summary>
         /// <remarks>
